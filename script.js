fd7c3491dc876f99b88b15a383c57b04cb0accc8
Viharic <97176997+Viharic@users.noreply.github.com>
2024-11-03 11:06:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("australia-btn").onclick = function() {
+  window.location.href = "australia.html"; // Replace with actual Australia page URL
+};
+
+document.getElementById("canada-btn").onclick = function() {
+  window.location.href = "canada.html"; // Replace with actual Canada page URL
+};
+
+document.getElementById("nz-btn").onclick = function() {
+  window.location.href = "newzealand.html"; // Replace with actual New Zealand page URL
+};
+
+document.getElementById("hk-btn").onclick = function() {
+  window.location.href = "hongkong.html"; // Replace with actual Hong Kong page URL
+};
